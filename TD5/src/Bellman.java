631e4f2e45b189eac899aa3399d679a2c91233fa
Annie <annie1.rochette@hotmail.com>
2018-11-28 23:02:19 -0500

    Exercice 3
    J'ai aussi ajouté la fonction printPath()

--- TD5/src/Bellman.java
+++ TD5/src/Bellman.java
@@ -23,12 +23,73 @@ public class Bellman {
 	
 	public void shortestPath() {
 		// Compléter
-		
 	}
 	
 	public void  diplayShortestPaths() {
-		//Compléter
+		String affichage = "=> Les chemins sont: \n";
+		Stack<Node> path = new Stack<Node>();
+		Node origin;
+
+		for(int i = 1; i <= graph.getNodes().size(); i++){
+			origin = graph.getNodeById(i);
+			path.push(origin);
+
+			int nextID= rTable.get(rTable.size() - 1).get(path.peek().getId()-1);
+			if(nextID==0)
+				nextID++;
+			path.push(graph.getNodeById(nextID));
+
+			for(; path.peek() != sourceNode && path.peek() != origin; ){
+
+				nextID= rTable.get(rTable.size() - 1).get(path.peek().getId()-1);
+				if(nextID==0)
+					nextID++;
+				path.push(graph.getNodeById(nextID));
+			}
+
+			if(origin == sourceNode && path.size() == 2){
+				path.clear();
+			}
+			else if(path.peek() == origin){
+				System.out.print(printPath(i,path,true,origin));
+
+				return;
+			}
+			else{
+				affichage += printPath(i, path, false, origin);
+			}
+
+		}
+		System.out.print(affichage);
 	}
+
+	public String printPath(int index, Stack<Node> path, boolean negative, Node origin){
+
+
+		if(negative == false) {
+			String print = "[" + sourceNode.getName() + " - " + graph.getNodes().get(index - 1).getName() + "]" + piTable.get(piTable.size() - 1).get(index - 1)
+					+ " : " + path.pop().getName();
+
+			while (!path.isEmpty()) {
+				print += " -> " + path.pop().getName();
+			}
+			print += "\n";
+
+			return print;
+		}
+		else{
+			String print = "=> Le graphe contient un circuit de coût négatif: \n[" + origin.getName() + " - " + origin.getName() + "]";
+
+			while(!path.isEmpty()) {
+				if (path.size() == 1)
+					print += path.pop().getName();
+				else
+					print += path.pop().getName() + " -> ";
+			}
+			return print;
+		}
+	}
+
 
 	public void displayTables() {
 	 //Compléter
